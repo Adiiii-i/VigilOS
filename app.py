04b7821ba944c37c60e90550514a9a57c78d4700
Adiiii-i <aadis9028@gmail.com>
2026-07-13 20:35:57 +0530

    fix: change default fallback port to 7860 for Hugging Face compatibility

--- app.py
+++ app.py
@@ -93,5 +93,5 @@ def api_process_frame():
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 7860))
     app.run(host="0.0.0.0", port=port, debug=False, threaded=True)
